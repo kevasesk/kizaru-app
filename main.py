86f950d94b2473f5aff829902458867f1ee9d141
Your Name <you@example.com>
2022-02-10 11:41:54 +0200

    fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def downloadUpdate():
     with open(local_file, 'wb') as file:
         file.write(data.content)
 
-        
+
 remote_url = 'https://raw.githubusercontent.com/kevasesk/kizaru-app/master/version'
 local_file = 'version'
 data = requests.get(remote_url)
@@ -16,15 +16,15 @@ if data.status_code == 200:
     versionFileExist = os.path.exists('./version')
     if versionFileExist:
         f = open("version", "r")
-        if data.content != f.read():
+        fileText = f.read()
+        if data.text != fileText:
             downloadUpdate()
     else:
         downloadUpdate()
 
     import update
 
-print('before app')
-sys.exit()
+
 import app
 
 
--- update.py
+++ update.py
@@ -1,9 +1,5 @@
 import requests, sys
 
-
-print('executed update')
-sys.exit()
-
 remote_url = 'https://raw.githubusercontent.com/kevasesk/kizaru-app/master/app.py'
 local_file = 'app.py'
 data = requests.get(remote_url)
