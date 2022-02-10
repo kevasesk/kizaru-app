35f3889a8665d66d647ad0afef4ea14581d3743c
Your Name <you@example.com>
2022-02-10 11:09:47 +0200

    fixs

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import requests, sys, os
 
+def downloadUpdate():
+    remote_url = 'https://raw.githubusercontent.com/kevasesk/kizaru-app/master/update.py'
+    local_file = 'update.py'
+    data = requests.get(remote_url)
+    with open(local_file, 'wb') as file:
+        file.write(data.content)
+
+        
 remote_url = 'https://raw.githubusercontent.com/kevasesk/kizaru-app/master/version'
 local_file = 'version'
 data = requests.get(remote_url)
-
 
 if data.status_code == 200:
     versionFileExist = os.path.exists('./version')
     if versionFileExist:
         f = open("version", "r")
-        if data.content !== f.read():
+        if data.content != f.read():
             downloadUpdate()
     else:
         downloadUpdate()
@@ -21,9 +28,4 @@ sys.exit()
 import app
 
 
-def downloadUpdate:
-    remote_url = 'https://raw.githubusercontent.com/kevasesk/kizaru-app/master/update.py'
-    local_file = 'update.py'
-    data = requests.get(remote_url)
-    with open(local_file, 'wb') as file:
-        file.write(data.content)
+
--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-import requests
+import requests, sys
 
 
 print('executed update')
